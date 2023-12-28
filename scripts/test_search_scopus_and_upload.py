3f4bd53b8ecc04bfbd250ea62199407b0002098b
sonic160 <zhiguo.zeng@centralesupelec.fr>
2023-12-28 22:51:30 +0100

    test for stability

--- scripts/test_search_scopus_and_upload.py
+++ scripts/test_search_scopus_and_upload.py
@@ -7,10 +7,12 @@ if __name__ == '__main__':
     # Define search query.
     # search_query = 'AU-ID(55659850100) OR AU-ID(55348807500) OR AU-ID(7102745133) AND PUBYEAR > 2017 AND PUBYEAR < 2025 AND AFFIL (centralesupelec)'
     # search_query = 'AU-ID(55348807500) AND PUBYEAR > 2016 AND PUBYEAR < 2025'
-    search_query = 'AU-ID(7005289082) AND PUBYEAR > 2000  AND PUBYEAR < 2018 AND (AFFIL (centralesupelec) OR AFFIL (Supelec))' # Enrico Zio
+    # search_query = 'AU-ID(7005289082) AND PUBYEAR > 2000  AND PUBYEAR < 2025 AND (AFFIL (centralesupelec) OR AFFIL (Supelec))' # Enrico Zio
     # search_query = 'AU-ID(6602469780) AND PUBYEAR > 2000 AND PUBYEAR < 2025 AND AFFIL (centralesupelec)' # Bernard Yannou
     # search_query = 'AU-ID(56609542700) AND PUBYEAR > 2000 AND PUBYEAR < 2025 AND (AFFIL (centralesupelec) OR AFFIL (Supelec))' # Yanfu Li
-    
+    # search_query = 'AU-ID(14049106600) AND PUBYEAR > 2000  AND PUBYEAR < 2025 AND (AFFIL (centralesupelec) OR AFFIL (Supelec))' # Nicola Pedroni
+    search_query = 'AU-ID(7102745133) AND PUBYEAR > 2000 AND PUBYEAR < 2025' # Anne Barros
+
     results = ScopusSearch(search_query, view='COMPLETE', refresh=True)
     df_result = pd.DataFrame(results.results)
     df_result.to_csv('./data/outputs/scopus_search_results.csv', index=False)
@@ -26,7 +28,7 @@ if __name__ == '__main__':
     # Define the stamps you want to add to the paper.
     # If you don't want to add stamp: stamps = []
     # stamps = ['LGI-SR', 'CHAIRE-RRSC']
-    stamps = ['LGI-SR'] # Add your stamps here
+    stamps = [] # Add your stamps here
 
     # # For debugging: Only upload the first rowRange records.
     # # Comment this line if you want to upload all the records.
